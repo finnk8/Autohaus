707138e7ab8afc23df095602c077c299c49d4a2e
finnk8 <finn.klusmann@gmx.de>
2022-11-30 14:29:06 +0100

    update

--- autohaus.py
+++ autohaus.py
@@ -1,52 +1,84 @@
 import random
+import datetime
+
 class Auto:
-    def __init__(self, marke, modell, preis, besitzer):
+    def __init__(self, marke, modell, lakierung, preis, besitzer):
         self.marke = marke
         self.modell = modell
+        self.lakierung = lakierung
         self.id = None
         self.tuev = None
+        self.naechster_tuev = None
+        self.schaeden_liste = []
         self.preis = preis
         self.besitzer = besitzer
         self.autohaus = None
 
-    def update(self, **kwargs):
-        pass 
+    def umlakieren(self, farbe):
+        self.lakierung = str(farbe)
+
+    def set_tuev(self):
+        self.tuev = datetime.date.today() + datetime.timedelta(weeks=104)
+
+    def add_schaden(self, schaden):
+        self.schaeden_liste.append(str(schaden))
+
+class Tesla(Auto):
+    def __init__(self, modell, preis, besitzer):
+        super().__init__(modell, preis, besitzer)
+        self.marke = "Tesla"
+        self.akku_tausch = None
+
+    def set_akku_tausch(self):
+        self.akku_tausch = datetime.date.today()
 
 Markt = []
 r = random.SystemRandom()
 for i in range(25):
-    Markt.append(Auto('Audi', 'TT', r.randint(4000,26000), 'Tom'))
+    Markt.append(Auto('Audi', 'TT', 'rot', r.randint(4000,26000), 'Tom'))
 
 class Autohaus:
     def __init__(self, name, standort):
         self.name = name
         self.standort = standort
         self.kontostand = 0
-        self.autos = [Auto('Audi', 'TT', 10000, 'Henry')]
+        self.autos = [Auto('Audi', 'TT', 'blau', 10000, 'Henry')]
         self.verkaefer = {}
         self.ankaefer = {}
 
-    def verkauf(self, auto: Auto):
+    def verkauf(self, auto_id):
+        auto = self.autos[auto_id]
         self.kontostand += auto.preis
         autos = self.autos
-        autos.remove(myautohaus.autos[0])
         Markt.append(auto)
+        autos.remove(auto)
     
-    def ankauf(self, auto: Auto):
+    def ankauf(self, auto_id):
+        auto = Markt[auto_id]
         self.kontostand -= auto.preis
         autos = self.autos
-        Markt.remove(auto)
         autos.append(auto)
+        Markt.remove(auto)
 
-myautohaus = Autohaus("Autohaus - Klusmann","Hamburg")
+myautohaus = Autohaus("Autohaus - Finn und Tore","Hamburg")
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
-def autohaus():
+def index():
     return render_template('index.html', autohaus=myautohaus, markt=Markt)
 
-if "__main__" == __name__:
+@app.route('/verkaufen/<id>')
+def verkaufen(id):
+    myautohaus.verkauf(int(id))
+    return redirect(url_for('index'))
+
+@app.route('/ankaufen/<id>')
+def ankaufen(id):
+    myautohaus.ankauf(int(id))
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
     app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,3 @@
+import datetime
+date_1 = datetime.date.today()
+print(date_1 + datetime.timedelta(weeks=104))
