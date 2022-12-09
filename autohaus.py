57ad9430422ec33ac8c3b8e94fa5d919a297b4cb
finnk8 <finn.klusmann@gmx.de>
2022-12-09 19:19:10 +0100

    worked on everything

--- autohaus.py
+++ autohaus.py
@@ -1,14 +1,26 @@
 import random
 import datetime
+import names
+
+r = random.SystemRandom()
+
+def random_date(start, end):
+    """
+    This function will return a random datetime between two datetime 
+    objects.
+    """
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = r.randrange(int_delta)
+    return start + datetime.timedelta(seconds=random_second)
 
 class Auto:
-    def __init__(self, marke, modell, lakierung, preis, besitzer):
+    def __init__(self, marke, modell, lakierung, tuev, preis, besitzer):
         self.marke = marke
         self.modell = modell
         self.lakierung = lakierung
         self.id = None
-        self.tuev = None
-        self.naechster_tuev = None
+        self.tuev = tuev
         self.schaeden_liste = []
         self.preis = preis
         self.besitzer = besitzer
@@ -16,9 +28,11 @@ class Auto:
 
     def umlakieren(self, farbe):
         self.lakierung = str(farbe)
+        self.preis += int(self.preis*0.02)
 
     def set_tuev(self):
-        self.tuev = datetime.date.today() + datetime.timedelta(weeks=104)
+        self.tuev = str(datetime.date.today() + datetime.timedelta(weeks=104))
+        self.preis += int(self.preis*0.02)
 
     def add_schaden(self, schaden):
         self.schaeden_liste.append(str(schaden))
@@ -33,18 +47,21 @@ class Tesla(Auto):
         self.akku_tausch = datetime.date.today()
 
 Markt = []
-r = random.SystemRandom()
+
+auto_marken = ['Audi', 'Ford', 'Mercedes Benz', 'VW', 'Skoda', 'Fiat', 'Mini', 'Smart']
+auto_modelle = ['TT', 'A3', 'Columbus', 'Cooper', 'S250', 'T40', 'Golf', 'Yeti']
+auto_farbe = ['red', 'blue', 'green', 'yellow', 'white', 'black', 'grey']
+tree_years_ago = datetime.date.today() - datetime.timedelta(weeks=104)
 for i in range(25):
-    Markt.append(Auto('Audi', 'TT', 'rot', r.randint(4000,26000), 'Tom'))
+    Markt.append(Auto(str(r.choice(auto_marken)), str(r.choice(auto_modelle)), str(r.choice(auto_farbe)), random_date(tree_years_ago, datetime.date.today()), r.randint(4000,26000), str(names.get_first_name())))
 
 class Autohaus:
     def __init__(self, name, standort):
         self.name = name
         self.standort = standort
         self.kontostand = 0
-        self.autos = [Auto('Audi', 'TT', 'blau', 10000, 'Henry')]
-        self.verkaefer = {}
-        self.ankaefer = {}
+        self.autos = [Auto('Audi', 'TT', 'blau', random_date(tree_years_ago, datetime.date.today()), 10000, 'Henry')]
+        self.kunden = []
 
     def verkauf(self, auto_id):
         auto = self.autos[auto_id]
@@ -58,11 +75,19 @@ class Autohaus:
         self.kontostand -= auto.preis
         autos = self.autos
         autos.append(auto)
+        self.kunden.append(auto.besitzer)
         Markt.remove(auto)
+
+    def auto_umlakieren(self):
+        if self.kontostand-50 >= 0:
+            self.kontostand -= 50
+            return True
+        else:
+            return False
 
 myautohaus = Autohaus("Autohaus - Finn und Tore","Hamburg")
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -78,6 +103,19 @@ def verkaufen(id):
 @app.route('/ankaufen/<id>')
 def ankaufen(id):
     myautohaus.ankauf(int(id))
+    return redirect(url_for('index'))
+
+@app.route('/umlakieren/<id>', methods=['GET', 'POST'])
+def umlakieren(id):
+    auto = myautohaus.autos[int(id)]
+    if myautohaus.auto_umlakieren() == True:
+        auto.umlakieren(request.args.get("farbe"))
+    return redirect(url_for('index'))
+
+@app.route('/tuev_verlaengern/<id>', methods=['GET', 'POST'])
+def tuev_verlaengern(id):
+    auto = myautohaus.autos[int(id)]
+    auto.set_tuev()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
